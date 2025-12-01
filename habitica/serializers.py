08bb166b9e4a97c9d7f4f2998a501628dfd6d561
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-01 23:09:46 +0300

    add serializers.py (add HabitSerializer)

---
+++ habitica/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Habit
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Habit"""
+
+    class Meta:
+        model = Habit
+        fields = '__all__'
+
+    def validate(self, data):
+        related_habit = data.get('related_habit')
+
+        reward = data.get('reward')
+        if related_habit and reward:
+            raise serializers.ValidationError(
+                "Нельзя одновременно указывать и связанную привычку и вознаграждение"
+            )
+
+        time_to_do = data.get('time_to_do')
+        if time_to_do and time_to_do > 120:
+            raise serializers.ValidationError(
+                "Время выполнения должно быть не больше 120 секунд."
+            )
+
+        if related_habit and not related_habit.is_good:
+            raise serializers.ValidationError(
+                "Связанная привычка должна быть приятной"
+            )
+
+        is_good = data.get('is_good', self.instance.is_good if self.instance else False)
+
+        if is_good and reward:
+            raise serializers.ValidationError(
+                "Приятная привычка не может иметь вознаграждение"
+            )
+        if is_good and related_habit:
+            raise serializers.ValidationError(
+                "Приятная привычка не может иметь связанную привычку"
+            )
+
+        return data
