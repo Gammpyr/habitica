9ea6f858fb9356d2063d615bc53a6748e88f3456
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-04 00:25:13 +0300

    update user.models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class CustomUser(models.Model):
-    pass
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.username} - {self.email}"
