010d49a4c9121fa5bb1e4f3f9e071c03a2093e1b
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-04 00:25:25 +0300

    update config.urls.py

--- config/urls.py
+++ config/urls.py
@@ -36,9 +36,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path('api-auth/', include(('rest_framework.urls', 'rest_framework'), namespace='rest_framework')),
+
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    path('', include('habitica.urls', namespace='habits')),
     path('users/', include('users.urls', namespace='users')),
-
 ]
