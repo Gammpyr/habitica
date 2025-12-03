fc622edf20510569b7e161ddc4e692e86ab520bf
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-04 00:25:18 +0300

    update user.urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+app_name = 'users'
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
