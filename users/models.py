f859ea763fb969633372aa6f08b9e290cf3d3029
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-06 00:00:44 +0300

    update users.models.py

--- users/models.py
+++ users/models.py
@@ -3,13 +3,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
-    USERNAME_FIELD = 'username'
+    telegram_chat_id = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="ID чата в Telegram"
+    )
+
+    USERNAME_FIELD = "username"
     # REQUIRED_FIELDS = ['username']
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return f"{self.username} - {self.email}"
