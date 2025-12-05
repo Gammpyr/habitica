fe400dc74d1f374a4683ddb40878d6c3d6f63fbb
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-06 00:00:12 +0300

    update celery.py settings

--- config/celery.py
+++ config/celery.py
@@ -1,19 +1,25 @@
-from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 
-# Установка переменной окружения для настроек проекта
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# import eventlet
+#
+# eventlet.monkey_patch(
+#     os=True,
+#     select=True,
+#     socket=True,
+#     thread=True,
+#     time=True,
+#     psycopg=False,
+# )
 
-# Создание экземпляра объекта Celery
-app = Celery('config')
 
-# Загрузка настроек из файла Django
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
+app = Celery("config")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-#@app.task(bind=True, ignore_result=True)
-#def debug_task(self):
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
 #    print(f'Request: {self.request!r}')
