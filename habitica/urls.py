9cc5bc836bd631742c45e3202494179a0a06ad20
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-06 00:01:59 +0300

    update habitica.urls.py

--- habitica/urls.py
+++ habitica/urls.py
@@ -1,17 +1,16 @@
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from habitica.views import HabitViewSet
 
-app_name = 'habitica'
+app_name = "habitica"
 
 router = DefaultRouter()
-router.register(r'habits', HabitViewSet, basename='habits')
+router.register(r"habits", HabitViewSet, basename="habits")
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('', include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include(router.urls)),
 ] + router.urls
