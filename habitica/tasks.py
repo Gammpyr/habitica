3c1fa2161ee2da0b12be557c333a33093d8744ab
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-07 20:35:35 +0300

    update habitica.tasks.py (add periodocoty check)

--- habitica/tasks.py
+++ habitica/tasks.py
@@ -20,14 +20,12 @@ def check_and_send_habit_reminders():
     """Основная задача: проверка и отправка напоминаний"""
     now_local = timezone.localtime()
     now = now_local.time()
-    # now = timezone.now().time()
 
     habits = Habit.objects.filter(
         time__hour=now.hour, time__minute=now.minute
     ).select_related("user")
 
     for habit in habits:
-
         if habit.user.telegram_chat_id:
             message = f"""
 Напоминание!
@@ -39,10 +37,28 @@ def check_and_send_habit_reminders():
 Место: {habit.place}
 Время на выполнение: {habit.time_to_do} секунд!
 """
+            periodicity = {
+                "day": 1,
+                "two_days": 2,
+                "three_days": 3,
+                "four_days": 4,
+                "five_days": 5,
+                "six_days": 6,
+                "week": 7,
+            }
+
+            required_interval = periodicity.get(habit.periodicity)
+            if required_interval:
+                days_since_last = (now_local.date() - habit.last_notified.date()).days
+
+                if days_since_last < required_interval:
+                    continue
 
             send_telegram_notification.delay(
                 chat_id=habit.user.telegram_chat_id, message=message
             )
+            habit.last_notified = timezone.now()
+            habit.save()
             print(f"Напоминание отправлено для привычки: {habit.action}")
         else:
             print(f"У пользователя {habit.user.username} нет telegram_chat_id ")
