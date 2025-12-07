ec21473bf9c32c8f52ae36bca3d22af4c5692367
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-12-07 20:35:10 +0300

    update habitica.services.py

--- habitica/services.py
+++ habitica/services.py
@@ -1,23 +1,22 @@
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 from config.settings import TELEGRAM_BOT_TOKEN, TELEGRAM_URL
 
 
 def send_telegram_message(chat_id, message):
-    params = {
-        "text": message,
-        "chat_id": chat_id,
-    }
-    response = requests.get(
-        f"{TELEGRAM_URL}{TELEGRAM_BOT_TOKEN}/sendMessage", params=params
-    )
-    return response
+    try:
+        params = {
+            "text": message,
+            "chat_id": chat_id,
+        }
+        response = requests.get(
+            f"{TELEGRAM_URL}{TELEGRAM_BOT_TOKEN}/sendMessage", params=params
+        )
+        return response
+    except Exception as e:
+        logger.error(f"Ошибка отправки в Telegram: {e}", exc_info=True)
+        return None
 
-
-#
-# def check_if_need_send_reminder(habit, current_hour, current_minute):
-#     """Проверка, пришло ли время отправлять напоминание для привычки"""
-#     habit_hour = habit.time.hour
-#     habit_minute = habit.time.minute
-#
-#     return habit_hour == current_hour and habit_minute == current_minute
